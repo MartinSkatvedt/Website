7f4fc674cd1dd8a89ef925f84b37655a2dbb3232
Martinskatvedt <martinskatvedt@gmail.com>
2021-06-18 03:18:11 +0200

    added pdf-loader and loaded CV

--- pages/CV.tsx
+++ pages/CV.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from "react";
-
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { Box, Heading } from "@chakra-ui/react";
 const CV: FC = () => {
-  return <div>CV page</div>;
+  return (
+    <Box w="100%" textAlign="center">
+      <Heading>CV</Heading>
+      <Box marginLeft="auto" marginRight="auto" width="45%">
+        <Document loading="Loading PDF" file="/Resume.pdf">
+          <Page pageNumber={1} />
+        </Document>
+      </Box>
+    </Box>
+  );
 };
 
 export default CV;
