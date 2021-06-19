dc0047a95d0c2adf1b63524a529a26a82d34cda2
Martinskatvedt <martinskatvedt@gmail.com>
2021-06-19 17:57:24 +0200

    finished CV page

--- components/Footer/Others.tsx
+++ components/Footer/Others.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { VStack, Text, Link } from "@chakra-ui/react";
+import { VStack, Text, Link, textDecoration } from "@chakra-ui/react";
 
 const Others: FC = () => {
   return (
@@ -8,6 +8,7 @@ const Others: FC = () => {
         <b>ANDRE</b>
       </Text>
       <Link
+        textDecoration="none"
         target="_blank"
         href="https://www.linkedin.com/in/martin-skatvedt-47597511a/"
       >
@@ -15,16 +16,21 @@ const Others: FC = () => {
           Linkedin
         </Text>
       </Link>
-      <Link target="_blank" href="https://github.com/MartinSkatvedt">
-        <Text fontSize="1xl" display="block" textDecoration="none">
+      <Link
+        target="_blank"
+        href="https://github.com/MartinSkatvedt"
+        textDecoration="none"
+      >
+        <Text fontSize="1xl" display="block">
           Github
         </Text>
       </Link>
       <Link
+        textDecoration="none"
         target="_blank"
         href="https://stackoverflow.com/users/16038209/martin-skatvedt?tab=profile"
       >
-        <Text fontSize="1xl" display="block" textDecoration="none">
+        <Text fontSize="1xl" display="block">
           Stack overflow
         </Text>
       </Link>
--- pages/CV.tsx
+++ pages/CV.tsx
@@ -1,13 +1,25 @@
 import React, { FC } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text, Link } from "@chakra-ui/react";
 const CV: FC = () => {
   return (
     <Box w="100%" textAlign="center">
       <Heading>CV</Heading>
-      <Box marginLeft="auto" marginRight="auto" width="45%">
-        <Document loading="Loading PDF" file="/Resume.pdf">
+      <Text>
+        Du kan se den faktiske PDFen{" "}
+        <Link color="orange.400" href="/Resume.pdf">
+          her
+        </Link>
+      </Text>
+
+      <Box
+        width="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Document loading="Loading PDF" file="/Resume.pdf" renderMode="canvas">
           <Page pageNumber={1} />
         </Document>
       </Box>
